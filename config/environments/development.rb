424ae6094d81880dbfabfbd404be5e633c17f304
Umair Abid <umairac90@gmail.com>
2024-06-07 20:47:41 -0700

    Initial Setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,5 +76,9 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
+
   config.assets.debug = true
 end
