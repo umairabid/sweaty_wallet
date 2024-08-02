f218d46929a72006c5eb712cb1eaac12d698a53b
Umair Abid <umairac90@gmail.com>
2024-08-01 19:10:28 -0700

    Add Filters For Transctions WIP

--- main_app/app/controllers/transactions_controller.rb
+++ main_app/app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
   def index
-    scope = current_user.transactions.preload(account: [:connector])
+    scope = current_user.transactions.order(date: :desc).preload(account: [:connector])
     @transactions = set_page_and_extract_portion_from scope
   end
 end
