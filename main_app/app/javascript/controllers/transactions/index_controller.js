7898c2a31888bbe597c79e4e38ff92ad282ae09b
Umair Abid <umairac90@gmail.com>
2025-01-18 15:36:19 -0800

    WIP transactions

--- main_app/app/javascript/controllers/transactions/index_controller.js
+++ main_app/app/javascript/controllers/transactions/index_controller.js
@@ -1,9 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 import create_drawer from "lib/create_drawer"
+import create_modal from "lib/create_modal"
 
 export default class extends Controller {
   connect() {
     this.init_search_drawer()
+    this.init_columns_modal()
   }
 
   open_search() {
@@ -11,6 +13,18 @@ export default class extends Controller {
   }
 
   init_search_drawer() {
-    this.advance_search_drawer = create_drawer(`advance-search-drawer`)
+    this.advance_search_drawer = create_drawer(`advance_search_drawer`)
+  }
+
+  init_columns_modal() {
+    this.columns_modal = create_modal("select_columns_modal");
+  }
+
+  close_columns_modal() {
+    this.columns_modal.hide()
+  }
+
+  open_columns_modal() {
+    this.columns_modal.show()
   }
 }
--- main_app/app/models/transaction.rb
+++ main_app/app/models/transaction.rb
@@ -1,4 +1,15 @@
 class Transaction < ApplicationRecord
+  COLUMNS = {
+    id: "ID",
+    date: "Date",
+    external_id: "External ID",
+    description: "Description",
+    amount: "Amount",
+    type: "Type",
+    category_name: "Category",
+    account_name: "Account",
+    bank_name: "Bank",
+  }
   belongs_to :account
   belongs_to :category, optional: true
 
