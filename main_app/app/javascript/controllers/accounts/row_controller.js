4e1d8f8405765e3a71d099abcdae4dcfc9597fc6
Umair Abid <umairac90@gmail.com>
2025-04-20 12:31:05 -0700

    WIP connectors page

--- main_app/app/javascript/controllers/accounts/row_controller.js
+++ main_app/app/javascript/controllers/accounts/row_controller.js
@@ -1,26 +1,28 @@
-import BaseController from "controllers/base_controller"
-import create_drawer from "lib/create_drawer"
+import BaseController from "controllers/base_controller";
+import create_drawer from "lib/create_drawer";
 
 export default class extends BaseController {
   connect() {
-    this.removeBackdrop()
+    this.removeBackdrop();
   }
 
   open_drawer() {
-    this.get_drawer().show()
+    console.log(this.get_drawer());
+    this.get_drawer().show();
   }
 
   close_drawer() {
-    this.get_drawer().hide()
+    console.log(this.get_drawer());
+    this.get_drawer().hide();
   }
 
   get_drawer() {
-    if (this.drawer) return this.drawer
-    this.drawer = create_drawer(`drawer-account-form-${this.account_id()}`)
-    return this.drawer
+    if (this.drawer) return this.drawer;
+    this.drawer = create_drawer(`drawer-account-form-${this.account_id()}`);
+    return this.drawer;
   }
 
   account_id() {
-    return this.element.dataset.account_id
+    return this.element.dataset.account_id;
   }
 }
