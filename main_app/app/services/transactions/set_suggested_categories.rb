fa8c48872c753322b58b24b6ebc71f006324b280
umairabid <umairac90@gmail.com>
2025-07-04 20:13:18 -0700

    Set suggested category service

---
+++ main_app/app/services/transactions/set_suggested_categories.rb
@@ -0,0 +1,77 @@
+class Transactions::SetSuggestedCategories
+  include Callable
+
+  def initialize(user, transaction_ids)
+    @user = user
+    @transaction_ids = transaction_ids
+  end
+
+  def call
+    chat = RubyLLM.chat.with_request_options(generationConfig: schema)
+    content = chat.ask(prompt).content
+    suggestions = JSON.parse(content)
+    transactions_by_id = transactions.index_by(&:id)
+    suggestions.each do |suggestion|
+      transaction = transactions_by_id[suggestion['id']]
+      transaction.suggested_category = categories_by_code[suggestion['category']]
+      transaction.save!
+    end
+  end
+
+  def schema
+    {
+      responseMimeType: 'application/json',
+      responseSchema: {
+        type: 'ARRAY',
+        items: {
+          type: 'OBJECT',
+          properties: {
+            id: { type: 'NUMBER' },
+            category: { type: 'STRING' }
+          }
+        }
+      }
+    }
+  end
+
+  def categories_by_code
+    @categories_by_code ||= @user.categories.index_by(&:code)
+  end
+
+  def transactions_prompt
+    transactions.map do |transaction|
+      {
+        id: transaction.id,
+        description: transaction.description,
+        neighbors: transaction.to_neighbors.map do |neighbor|
+          {
+            description: neighbor.description,
+            category: neighbor.category.code
+          }
+        end
+      }
+    end
+  end
+
+  def transactions
+    @transactions ||= @user.transactions
+      .where(id: @transaction_ids, suggested_category: nil)
+      .preload(to_neighbors: :category)
+  end
+
+  def prompt
+    <<~PROMPT
+      I am providing you with the user categories and transaction descriptions that needs to be categorized in one of user categories
+      To set the context, with each description I will provide you transaction_id, to identify transaction and neighbors that are 
+      closest to the description and are already categorized, each transaction will be in the following format
+      { id: 1, description: 'Walmart Charge', neighbors: [ {description: 'Other Walmart Charge', category: 'groceries'} ] }
+
+      Here are user categories
+      #{categories_by_code.keys.join("\n")}
+
+      Here are transactions
+      #{transactions_prompt}
+    PROMPT
+  end
+end
+
---
+++ main_app/spec/services/transactions/set_suggested_categories_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Transactions::SetSuggestedCategories, type: :service do
+  let(:user) { create(:user) }
+  let(:connector) { create(:connector, user: user) }
+  let(:account) { create(:account, connector: connector) }
+  let(:transaction) do 
+    transaction = create(:transaction, account: account)
+    transaction.to_neighbors << create(:transaction, account: account, category: user.categories.sample)
+    transaction.to_neighbors << create(:transaction, account: account, category: user.categories.sample)
+    transaction.to_neighbors << create(:transaction, account: account, category: user.categories.sample)
+    transaction.save!
+    transaction
+  end
+  let(:transaction_ids) { [transaction.id] }
+  let(:service) { Transactions::SetSuggestedCategories.new(user, transaction_ids) }
+  let(:suggested_category) { user.categories.sample }
+  
+  before do
+    expect(RubyLLM).to receive_message_chain(:chat, :with_request_options, :ask, :content)
+      .and_return([{ id: transaction.id, category: suggested_category.code }].to_json)
+  end
+  
+  context 'when suggestions are provided' do
+    it 'sets suggested categories' do
+      service.call
+      expect(transaction.reload.suggested_category_id).to eq(suggested_category.id)
+    end
+  end
+end
+
