0652cc365d25ca2618985018ec54a8940dc25e69
Umair Abid <umairac90@gmail.com>
2025-08-17 13:45:00 -0700

    Upgrading to 8.0 v1

--- main_app/app/models/account.rb
+++ main_app/app/models/account.rb
@@ -15,7 +15,7 @@ class Account < ApplicationRecord
 
   FILTERABLE_ACCOUNT_TYPES = %i[credit_card deposit_account].freeze
 
-  enum account_type: { credit_card: 1, deposit_account: 2, investment: 3, credit_line: 4,
+  enum :account_type, { credit_card: 1, deposit_account: 2, investment: 3, credit_line: 4,
                        mortgage: 5 }
   validates :external_id, uniqueness: { scope: :connector }
 
