22f777e7bd10e80b1739adcaff0cd6811e01f010
Umair Abid <umairac90@gmail.com>
2025-08-17 16:00:30 -0700

    Fix spec

--- main_app/spec/services/transactions/set_suggested_categories_spec.rb
+++ main_app/spec/services/transactions/set_suggested_categories_spec.rb
@@ -4,23 +4,26 @@ RSpec.describe Transactions::SetSuggestedCategories, type: :service do
   let(:user) { create(:user) }
   let(:connector) { create(:connector, user: user) }
   let(:account) { create(:account, connector: connector) }
-  let(:transaction) do 
+  let(:transaction) do
     transaction = create(:transaction, account: account)
-    transaction.to_neighbors << create(:transaction, account: account, category: user.categories.sample)
-    transaction.to_neighbors << create(:transaction, account: account, category: user.categories.sample)
-    transaction.to_neighbors << create(:transaction, account: account, category: user.categories.sample)
+    transaction.to_neighbors << create(:transaction, account: account,
+      category: user.categories.sample)
+    transaction.to_neighbors << create(:transaction, account: account,
+      category: user.categories.sample)
+    transaction.to_neighbors << create(:transaction, account: account,
+      category: user.categories.sample)
     transaction.save!
     transaction
   end
   let(:transaction_ids) { [transaction.id] }
   let(:service) { Transactions::SetSuggestedCategories.new(user, transaction_ids) }
   let(:suggested_category) { user.categories.sample }
-  
+
   before do
-    expect(RubyLLM).to receive_message_chain(:chat, :with_request_options, :ask, :content)
+    expect(RubyLLM).to receive_message_chain(:chat, :with_params, :ask, :content)
       .and_return([{ id: transaction.id, category: suggested_category.code }].to_json)
   end
-  
+
   context 'when suggestions are provided' do
     it 'sets suggested categories' do
       service.call
@@ -28,4 +31,3 @@ RSpec.describe Transactions::SetSuggestedCategories, type: :service do
     end
   end
 end
-
