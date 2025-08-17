935e32ffec2a405e0cf92f33ede28cab8d6dd865
Umair Abid <umairac90@gmail.com>
2025-08-17 14:04:37 -0700

    Fix specs

--- main_app/app/models/file_import.rb
+++ main_app/app/models/file_import.rb
@@ -2,5 +2,5 @@ class FileImport < ApplicationRecord
   belongs_to :user
   has_one_attached :file
 
-  enum status: { pending: 1, processing: 2, success: 3, failed: 4 }
+  enum :status, { pending: 1, processing: 2, success: 3, failed: 4 }
 end
--- main_app/spec/rails_helper.rb
+++ main_app/spec/rails_helper.rb
@@ -1,10 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require "spec_helper"
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require "rspec/rails"
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -62,5 +62,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   #
+  Rails.application.try(:reload_routes_unless_loaded)
   config.include Devise::Test::ControllerHelpers, type: :controller
 end
