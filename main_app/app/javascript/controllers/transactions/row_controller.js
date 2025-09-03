6703ff6e95b38388f8b4950fafa215123e313064
Umair Abid <umairac90@gmail.com>
2025-09-02 18:11:24 -0700

    Fixes #21 and #22

--- main_app/app/javascript/controllers/transactions/row_controller.js
+++ main_app/app/javascript/controllers/transactions/row_controller.js
@@ -29,18 +29,36 @@ export default class extends BaseController {
     event.preventDefault()
     const button = event.currentTarget.closest('button')
     const td = button.parentElement
-    const select = td.querySelector('select')
-    const form = select.form
-    const tom_select = this.get_autocomplete_select(select).enable()
-    form.classList.toggle('hidden')
-    button.classList.toggle('hidden')
+    this.toggle_category_form(td)
   }
 
   get_autocomplete_select(select) {
     if (this.autocomplete_select) return this.autocomplete_select     
-    this.autocomplete_select = new TomSelect(select)
+
+    const td = select.closest('td')
+    this.autocomplete_select = new TomSelect(select, {
+      onBlur: () => {
+        this.toggle_category_form(td)
+      },
+    })
     return this.autocomplete_select
   }
+
+  toggle_category_form(td) {
+    const select = td.querySelector('select')
+    const tom_select = this.get_autocomplete_select(select)
+    const form = select.form
+    const button = td.querySelector('button')
+    if (form.classList.contains('hidden')) {
+      tom_select.enable()
+      tom_select.focus()
+    } else {
+      this.get_autocomplete_select(select).disable()
+    }
+    form.classList.toggle('hidden')
+    button.classList.toggle('hidden')
+  }
+    
 
   update_category(event) {
     const select = event.currentTarget.closest('select')
