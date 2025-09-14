25995c15bb85633eb7f76611e41510eb03ebbac3
Umair Abid <umairac90@gmail.com>
2025-09-13 19:24:39 -0700

    Add rubocop (#51)
    * Add rubocop
    
    * Change working directory
    
    * Fix directory
    
    * Fix Steps
    
    * uhh
    
    * Test Dir
    
    * Please work
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Remove workdir
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Fix ruby version
    
    * For all files
    
    * Refactor
    
    * Test
    
    * Set working dir
    
    * Use rails path for rspec
    
    * Use Bundle

--- main_app/app/components/transactions/list_component.rb
+++ main_app/app/components/transactions/list_component.rb
@@ -6,15 +6,18 @@ class Transactions::ListComponent < ViewComponent::Base
     show_pagination: true
   }.freeze
 
-  def initialize(user:, columns: nil, filter: nil, page: 1, limit: Pagy::DEFAULT[:limit], options: DEFAULT_OPTIONS)
-    @params = params
-    @columns = columns || Transaction::DEFAULT_COLUMNS.map { |k| [k, '1'] }.to_h
-    @columns['suggested_category'] = '1' if options[:with_suggestions]
-    @filter = filter
-    @page = page
-    @user = user
-    @options = options
-    @limit = limit
+  def initialize(**args)
+    puts args.inspect
+    @options = args[:options] || DEFAULT_OPTIONS
+    
+    @columns = args[:columns] || Transaction::DEFAULT_COLUMNS.to_h { |k| [k, '1'] }
+    @columns['suggested_category'] = '1' if @options[:with_suggestions]
+    
+    @filter = args[:filter]
+    @page = args[:page] || 1
+    @user = args[:user]
+    @limit = args[:limit] || Pagy::DEFAULT[:limit]
+    @sort = args[:sort] || { date: :desc, id: :asc }
     set_user_references
   end
 
@@ -25,13 +28,15 @@ class Transactions::ListComponent < ViewComponent::Base
   end
 
   def params
-    { page: [@page.to_i || 0, 1].max }
+    { page: [@page.to_i, 1].max }
   end
 
   def set_transactions
     scope = @user.transactions.preload(:account)
     scope = Transactions::ScopeBuilder.call(@filter, scope) if @filter.present?
-    @pagy, @transactions = pagy_countless(scope, limit: @limit, request_path: helpers.transactions_path)
+    scope = scope.order(@sort)
+    @pagy, @transactions = pagy_countless(scope, limit: @limit,
+      request_path: helpers.transactions_path)
   end
 
   def user_repo
--- main_app/app/filters/transactions/scope_builder.rb
+++ main_app/app/filters/transactions/scope_builder.rb
@@ -25,7 +25,7 @@ class Transactions::ScopeBuilder
     @scope = @scope.where(is_credit: type == 'credit') if has? :type
     @scope = @scope.where(account_id:) if has? :account_id
     @scope = @scope.where(account: { account_type: }) if has? :account_type
-    @scope = @scope.order(date: :desc, id: :asc).preload(account: :connector)
+    @scope = @scope.preload(account: :connector)
 
     add_time_range if has? :time_range
     add_categories if has? :categories
